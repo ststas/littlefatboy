584c61ca87715856b041d816f5cbed4833598fe0
Stas Zaitsev <ststas@yandex.ru>
2023-08-18 13:57:14 +0300

    update script js

--- pages/script.js
+++ pages/script.js
@@ -13,35 +13,5 @@ const toggleHeaderMenu = () => {
 }
 
 // Set event listeners
-// const onHeaderMenuButtonClick = () => toggleHeaderMenu();
-// const onHeaderMenuOverlayClick = () => toggleHeaderMenu();
 headerMenuButtonContainerElement.addEventListener('click', () => toggleHeaderMenu());
 headerMenuOverlayElement.addEventListener('click', () => toggleHeaderMenu());
-
-// const navigationBlockElement = document.querySelector('.navigation__block');
-// const headerMenuElement = navigationBlockElement.querySelector('.navigation__menu');
-// const headerMenuButtonContainerElement = navigationBlockElement.querySelector('.navigation__menu-button-container');
-// const headerMenuButtonElement = headerMenuButtonContainerElement.querySelector('.navigation__menu-button')
-// const headerMenuOverlayElement = navigationBlockElement.querySelector('.navigation__menu-overlay');
-
-// //open & close menu functions
-// const openCloseHeaderMenu = () => {
-//   headerMenuElement.classList.toggle('navigation__menu_opened')
-// }
-// const openCloseMenuOverlay = () => {
-//   headerMenuOverlayElement.classList.toggle('navigation__menu-overlay_opened')
-// }
-// const clickNavigationMenuButton = () => {
-//   headerMenuButtonElement.classList.toggle('navigation__menu-button_clicked')
-// }
-// //set event listeners
-// headerMenuButtonContainerElement.addEventListener('click', () => {
-//   openCloseHeaderMenu();
-//   openCloseMenuOverlay();
-//   clickNavigationMenuButton();
-// })
-// headerMenuOverlayElement.addEventListener('click', () => {
-//   openCloseHeaderMenu();
-//   openCloseMenuOverlay();
-//   clickNavigationMenuButton();
-// });
